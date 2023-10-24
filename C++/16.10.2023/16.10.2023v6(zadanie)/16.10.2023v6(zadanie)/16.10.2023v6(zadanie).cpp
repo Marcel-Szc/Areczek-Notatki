c15514360d1b71c3bee2d1e4a1cbc65e807f4e02
Lester Crest PZS1 <61290250+Marcel-Szc@users.noreply.github.com>
2023-10-24 07:41:06 +0200

    Add files via upload

---
+++ C++/16.10.2023/16.10.2023v6(zadanie)/16.10.2023v6(zadanie)/16.10.2023v6(zadanie).cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+using namespace std;
+
+float iloczyn(int tablica[]);
+float iloraz(int tablica[]);
+float roznica(int tablica[]);
+float suma(int tablica[]);
+
+int main()
+{
+	int nazwa[] = { 1,2,3,4,5,6,7,8,9,10 };
+	cout << "suma elemntow tablicy = " << suma(nazwa);
+	cout << "suma elemntow tablicy = " << roznica(nazwa);
+	cout << "suma elemntow tablicy = " << iloczyn(nazwa);
+	cout << "suma elemntow tablicy = " << iloraz(nazwa);
+	return 0;
+	
+
+	
+}
+
+float iloczyn(int tablica[])
+{   
+	return 0;
+}
+
+float iloraz(int tablica[])
+{
+	return 0.0f;
+}
+
+float roznica(int tablica[])
+{
+	int suma = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		suma = suma * tablica[i];
+	}
+	return suma; 
+}
+
+float suma(int tablica[])
+{
+	int suma = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		suma = suma + tablica[i];
+	}
+	return suma;
+}
