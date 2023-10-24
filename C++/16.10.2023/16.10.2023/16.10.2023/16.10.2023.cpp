c39491ab6757368437cae2207837e078da20c31e
Lester Crest PZS1 <61290250+Marcel-Szc@users.noreply.github.com>
2023-10-24 07:20:05 +0200

    Add files via upload

---
+++ C++/16.10.2023/16.10.2023/16.10.2023/16.10.2023.cpp
@@ -0,0 +1,22 @@
+﻿// sprawdz za pomoca wyr skr. czy liczba podana miesci sie w zakresie
+// 1-10
+
+#include <iostream>
+using namespace std;
+
+void sprawdz(int liczba);
+int main()
+{
+	int liczba;
+	cout << "Podaj liczbe do sprawdzenia = "; cin >> liczba;
+	sprawdz(liczba);
+
+	system("pause");
+	return 0;
+}
+
+void sprawdz(int liczba)
+{
+	(liczba >= 1 && liczba <= 10) ? cout << "Ok " : cout << "Error ";
+	cout << endl;
+}
---
+++ C++/23.10.2023/Zadanie1/Zadanie1/Zadanie1.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string tablica[3];
+    cout << "Dodaj 3 imiona do tablicy: " << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << " Podaj " << i + 1 << " imie: "; cin >> tablica[i];
+    }
+    cout << "-----------------------------" << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        cout << tablica[i] << endl;
+    }
+    return 0;
+}  
