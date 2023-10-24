37c97a73fb766eeeb10488d26828a3c38cbebccd
Lester Crest PZS1 <61290250+Marcel-Szc@users.noreply.github.com>
2023-10-24 07:35:24 +0200

    Add files via upload

---
+++ C++/16.10.2023/16.10.2023v2/16.10.2023v2/16.10.2023v2.cpp
@@ -0,0 +1,28 @@
+﻿// sprawdz za pomoca wyr skr. czy liczba podana miesci sie w zakresie
+// 1-10
+
+#include <iostream>
+using namespace std;
+
+void sprawdz(int liczba);
+int main()
+{
+	int liczba;
+	cout << "Podaj ocene do sprawdzenia = "; cin >> liczba;
+	sprawdz(liczba);
+
+	system("pause");
+	return 0;
+}
+
+void sprawdz(int liczba)
+{
+	liczba == 6 ? cout << "celujacy"
+		: (liczba == 5 ? cout << "bdb"
+			: (liczba == 4 ? cout << "db"
+				: (liczba == 3 ? cout << "dost"
+					: (liczba == 2 ? cout << "dop"
+						: (liczba == 1 ? cout << "niedostateczny"
+							: cout << "nie ma takiej oceny")))));
+		  cout << endl;
+}
