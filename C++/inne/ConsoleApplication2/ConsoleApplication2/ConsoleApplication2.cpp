9b526b078b80a158398b4b45117cd04ac21ed0f7
Lester Crest PZS1 <61290250+Marcel-Szc@users.noreply.github.com>
2023-10-24 07:46:54 +0200

    Add files via upload

---
+++ C++/inne/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int zmienna = 3;
+	switch (zmienna)
+	{
+	case 1:
+		cout << "godosz po polsku " << endl;
+	case 2:
+		cout << "godosz po Izraelsku" << endl;
+	case 3:
+		cout << "godosz po Rumunsku " << endl;
+		break;
+	case 4:
+		cout << "godosz po angielsku chopie" << endl;
+	default:
+		cout << "godosz gupoty..." << endl;
+		break;
+	}
+}
