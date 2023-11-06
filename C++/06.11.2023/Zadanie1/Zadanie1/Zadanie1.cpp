5b6b1950426cca8c56dfddc19c7a2189496d32e7
Lester Crest PZS1 <61290250+Marcel-Szc@users.noreply.github.com>
2023-11-06 10:32:46 +0100

    Add files via upload

---
+++ C++/06.11.2023/Zadanie1/Zadanie1/Zadanie1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<fstream>
+using namespace std;
+// zadanie1: zapis do pliku txt linii wczytaniej z klawiatury
+int main()
+{
+	char znaki[100];
+	fstream plik;
+	plik.open("marcel.txt", ios::in | ios::out | ios::trunc);
+
+	cout << "podaj wpis: " << "\n";
+	cin.getline(znaki, 10, '\n');
+	plik << znaki;
+
+	plik.close();
+	system("pause");
+
+	EXIT_SUCCESS;
+   
+}
