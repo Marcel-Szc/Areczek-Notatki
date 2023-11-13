6c042a50219d88da62ab25f6cac37713bfef60b2
Lester Crest PZS1 <61290250+Marcel-Szc@users.noreply.github.com>
2023-11-13 10:32:47 +0100

    Add files via upload

---
+++ C++/13.11.2023/ZAD1/ZAD1/ZAD1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int tab1[10] = {7,3,1,6,9,5,4,10,2,2};
+int tab2[10];
+int tab3[10];
+int main()
+{
+    
+    
+    for (int i = 0; i < 5; i++)
+    {
+        cout << tab1[i] << endl;
+    };
+
+    tab1[6] = 12;
+    cout << tab1[6] << endl;
+
+    for (int i = 0; i < 10; i++) 
+    {
+       tab2[i] = tab1[i];
+    };
+    for (int i = 0; i < 10; i++)
+    {
+        tab3[i] = tab1[i] + tab2[i];
+    };
+    for (int i = 9; i > 0; i--)
+    {
+        tab2[i] = tab1[i];
+        cout << tab2[3];
+    };
+}
