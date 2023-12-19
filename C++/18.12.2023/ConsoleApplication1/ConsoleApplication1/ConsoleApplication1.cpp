2fb26a60cced5029a6dba2f31a60d3dcc090166e
Lester Crest PZS1 <61290250+Marcel-Szc@users.noreply.github.com>
2023-12-19 07:12:34 +0100

    Add files via upload

---
+++ C++/18.12.2023/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	stack <int> stosLiczby;
+
+	stosLiczby.push(10);
+	stosLiczby.push(20);
+	stosLiczby.push(30);
+	stosLiczby.push(40);
+	stosLiczby.push(50);
+	if (!stosLiczby.empty()) {stosLiczby.pop();}
+	else { cout << "stos pusty"; }
+
+	cout <<
+	
+
+	cout << stosLiczby.top();
+
+
+	return EXIT_SUCCESS;
+}
+
---
+++ C++/18.12.2023/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+struct DaneStruktura {
+    int liczba;
+    string opis;
+};
+class DaneKlasa {
+    private: 
+        DaneStruktura tablica[2];
+    public: 
+        DaneStruktura& PodajDane(int index) {
+            return tablica[index];
+        }
+
+
+};
+s
+
+
+int main()
+{
+    stack <DaneStruktura> mojaStruktura;
+    stack <DaneKlasa> mojaKlasa;
+
+    DaneStruktura pierwszyWpis = { 1,"Jeden" };
+    mojaStruktura.push(pierwszyWpis);
+
+
+    DaneStruktura DrugiWpis = { 2,"Dwa" };
+    mojaStruktura.push(DrugiWpis);
+
+
+
+    return 0;
+}
