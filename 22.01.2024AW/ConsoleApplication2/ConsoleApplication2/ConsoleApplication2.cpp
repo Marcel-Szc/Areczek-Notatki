d38c8f1fc8768db50ea91203439f671ae6db0178
Lester Crest PZS1 <61290250+Marcel-Szc@users.noreply.github.com>
2024-01-22 10:29:55 +0100

    Add files via upload

---
+++ 22.01.2024AW/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+struct Data {
+    unsigned int dd, mm, rr;
+};
+struct Osoba
+{
+    char imie[50];
+    char nazwisko[50];
+    string zawod;
+    int wiek;
+    Data data_ur;
+};
+
+int main()
+{
+
+    Osoba programista1 = {
+       "Lester",
+       "Crest",
+       "Napad na kasyno",
+       48,
+       {1, 1, 1975}
+    };
+
+    Osoba programista2 = {
+       "Tyler",
+       "Durden",
+       "Schizofrenia",
+       32,
+       {12, 12, 3000}
+    };
+
+    cout << "Programista nr 1 " << programista1.imie << "\n"
+        << programista1.nazwisko << "\n"
+        << programista1.zawod << "\n"
+        << programista1.wiek << "\n"
+        << programista1.data_ur.dd << programista1.data_ur.mm << programista1.data_ur.rr << endl;
+
+    cout << "Programista nr 1" << programista2.imie << "\n"
+        << programista2.nazwisko << "\n"
+        << programista2.zawod << "\n"
+        << programista2.wiek << "\n"
+        << programista2.data_ur.dd << programista2.data_ur.mm << programista2.data_ur.rr << endl;
+
+
+    return EXIT_SUCCESS;
+}
---
+++ 22.01.2024AW/Hobby ucznia/Hobby ucznia/Hobby ucznia.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+struct Data {
+    unsigned int dd, mm, rr;
+};
+struct Przedmioty {
+    char nazwa_pr[50];
+    char prowadzący[50];
+};
+struct Zainteresowania {
+    char Nazwa_Za[50];
+    int czas_Za;
+    char poziom_trud;
+};
+struct Uczeń
+{
+    char imie[50];
+    char nazwisko[50];
+    Przedmioty przedmioty;
+    Zainteresowania zainteresowania;
+    Data data_ur;
+};
+
+int main()
+{
+    /*
+     Uczeń uczeń1 = {
+       "Lester",
+       "Crest",
+       {"Angielski", "D. Libek"},
+       {"Napad na kasyno", "1800", "100/100"},
+       {1, 1, 1975}
+    };
+
+    Uczeń uczeń2 = {
+       "Tyler",
+       "Durden",
+       {"W-F", "Oszek"},
+       {"Walki", "2000", "90/100"},
+       {12, 12, 3000}
+    };
+
+    Uczeń uczeń3 = {
+       "Francisco",
+       "Lachowski",
+       {"Mogging", "Donald Trump"},
+       {"Walki", "45", "200/100"},
+       {12, 12, 3000}
+    };
+
+    cout << "Programista nr 1 " << uczeń1.imie << "\n"
+        << uczeń1.nazwisko << "\n"
+        << uczeń1.przedmioty.nazwa_pr << "\n"
+        << uczeń1.przedmioty.nazwa_pr << "\n"
+        << uczeń1.data_ur.dd << uczeń1.data_ur.mm << uczeń1.data_ur.rr << endl;
+
+    cout << "Programista nr 1" << programista2.imie << "\n"
+        << programista2.nazwisko << "\n"
+        << programista2.zawod << "\n"
+        << programista2.wiek << "\n"
+        << programista2.data_ur.dd << programista2.data_ur.mm << programista2.data_ur.rr << endl;
+        */
+
+    return EXIT_SUCCESS;
+}
