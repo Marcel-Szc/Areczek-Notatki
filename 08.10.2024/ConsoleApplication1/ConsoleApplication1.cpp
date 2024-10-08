22def09d1d749a537f391d5fac5d257e5343f2bf
Lester Crest PZS1 <61290250+Marcel-Szc@users.noreply.github.com>
2024-10-08 09:36:45 +0200

    Add files via upload

---
+++ 08.10.2024/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+/*
+class Zwierze {
+public:
+    Zwierze() { cout << "Zwierze stworzone" << endl; }
+    ~Zwierze() { cout << "Zwierze zniszczone" << endl; }
+}; */
+
+
+static class Matematyka {
+public:
+    static constexpr double PI = 3.14159;
+    static int silnia(int n) {
+        if (n <= 1) return 1;
+        return n * silnia(n - 1);
+    }
+};
+
+int main()
+{
+   /* cout << "Konstruktor i Destruktor\n";
+
+    Zwierze z;*/
+
+
+    return EXIT_SUCCESS;
+}
