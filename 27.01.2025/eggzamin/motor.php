37d7b31ded288ef63252cdfbc5811fce551c272f
MiloszGruszka <gruszkamilosz1@gmail.com>
2025-02-03 10:21:25 +0100

    lekcja

--- 27.01.2025/eggzamin/motor.php
+++ 27.01.2025/eggzamin/motor.php
@@ -5,18 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Motocykle</title>
-    <link rel="stylesheet" href="styl.css">
+    <style>
+    <?php include("styl.css") ?>
+    </style>
 </head>
 <body>
     <img src='motor.png' alt="motocykl"/>
     <header class="baner">
-        <h1>Motocykle - moja asja</h1>
+        <h1>Motocykle - moja pasja</h1>
     </header>
     <section class="lewy">
         <h2>Gdzie pojechać?</h2>
         <dl>
             <?php 
-                #...
+                $zapytanie2 = $polaczenie->query("SELECT nazwa, opis, poczatek, zrodlo FROM wycieczki INNER JOIN zdjecia ON zdjecia_id = zdjecia.id;");
+                while(list($nazwa,$opis,$poczatek,$zrodlo)=mysqli_fetch_row($zapytanie2)){
+                    echo("<dt>".$nazwa.", rozpoczyna się w ".$poczatek.", <a href=".$zrodlo.".jpg>zobacz zdjecie </a></dt>");
+                    echo("<dd>".$opis."<dd>");
+                }
             ?>
         </dl>
     </section>
@@ -32,9 +38,20 @@
     </section>
     <section class="prawy_2">
         <h2>Statystyki:</h2>
+        <p>Wpisanych wycieczek:
+            <?php 
+                $zapytanie3 = $polaczenie->query("SELECT COUNT(id) FROM wycieczki;");
+                foreach(mysqli_fetch_row($zapytanie3) as $ile){
+                    echo($ile);
+                }
+                
+            ?>
+        </p>
+        <p>Użytkowników forum: 200</p>
+        <p>Przesłanych zdjęć: 1300</p>
         
     </section>
-    <footer class="stopka">Stopka</footer>
+    <footer class="stopka">Stronę wykonał: 000000000000000</footer>
 
     
 </body>
